fc8a3995bb29bf402ca8122def02c36481918e9a
Nahé <nahema.hutin@gmail.com>
2022-07-02 11:48:17 +0200

    commit

---
+++ backend/models/sauce.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+//Création d'un shéma de données pour les sauces
+const sauceSchema = mongoose.Schema({
+  userId: { type: String, required: true },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true, minimum: 0, maximum: 10, default: 0 },
+  likes: { type: Number, required: false, default: 0 },
+  dislikes: { type: Number, required: false, default: 0 },
+  usersLiked: { type: [String], required: false },
+  usersDisliked: { type: [String], required: false },
+});
+
+module.exports = mongoose.model('Sauce', sauceSchema);
